4c383ea11a228786c97239654ee40103f194e977
mara38 <mspx2023@gmail.com>
2023-03-02 21:20:39 +0100

    Add functions and stuff

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 console.log("helloooooouuuu");
 // alert('hi');
-document.querySelector('button').addEventListener('click', () => {
-    location.href = "index.html";
-})
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('prev-page');
+    console.log('clicked');
+      btn.addEventListener('click', () => {
+        location.href='index.html';
+    });
+  });
+
+document.getElementById('change').addEventListener('click', () => {
+    document.body.style.backgroundColor="gray";
+    console.log(e);
+});
